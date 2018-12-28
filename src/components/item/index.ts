c119b0b8e78be9e314c65b1b7867557fb6fbd9dd
seoco <seokho.dev@gmail.com>
2018-12-29 00:07:25 +0900

    Add missing component

--- src/components/item/index.ts
+++ src/components/item/index.ts
@@ -1,4 +1,5 @@
 export { default as ItemPanel } from './ItemPanel';
+export { default as ItemPanelHeader } from './ItemPanelHeader';
 export { default as ItemViewer } from './ItemViewer';
 export { default as ItemEditor } from './ItemEditor';
 export { default as ItemEditorForm } from './ItemEditorForm';
